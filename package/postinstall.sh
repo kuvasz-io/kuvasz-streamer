9a46c4a26734252e1fa9256d848f9bc4df648ba0
Kuvasz <info@kuvasz.io>
2024-03-23 11:50:32 +0000

    add syslog conf

--- package/postinstall.sh
+++ package/postinstall.sh
@@ -5,4 +5,7 @@ fi
 if ! getent passwd "kuvasz" > /dev/null 2>&1 ; then
     useradd -r -g kuvasz -d /var/lib/kuvasz -s /sbin/nologin -c "kuvasz user" kuvasz
 fi
+touch /var/log/kuvasz-streamer.log
+chown syslog:adm /var/log/kuvasz-streamer.log
+systemctl restart rsyslog
 systemctl daemon-reload
